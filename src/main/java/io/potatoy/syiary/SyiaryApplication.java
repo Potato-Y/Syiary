3f93202bc37a718f084f0260a95634f3e45ea0d7
Potato-Y <projectpotatoy@gmail.com>
2023-07-12 23:43:07 +0900

    Create project

---
+++ src/main/java/io/potatoy/syiary/SyiaryApplication.java
@@ -0,0 +1,13 @@
+package io.potatoy.syiary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SyiaryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SyiaryApplication.class, args);
+    }
+
+}
